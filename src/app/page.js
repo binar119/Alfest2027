0745617f24e52b0b8ad97865a162cfcb23319613
Muhammad Multazam <aa.multazzam@gmail.com>
2026-07-08 21:42:13 +0700

    fix import path dan build timeline boss

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import TimelineEvent from '../components/TimelineEvent';
 import Image from "next/image"
 import Link from "next/link";
 import React, { useState, useEffect } from "react";
